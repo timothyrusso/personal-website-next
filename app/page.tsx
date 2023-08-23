9f099a29d68e599200e985e34c6346c8b8cbc9f0
Timothy Russo <russotimothy@live.it>
2023-08-23 18:27:10 +0200

    feat: add projects grid folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { Title } from '@/components/Title/Title'
 import { Description } from '@/components/Description/Description'
+import { ProjectsGrid } from '@/components/ProjectsGrid/ProjectsGrid'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col p-8">
       <Title />
       <Description />
+      <ProjectsGrid />
     </main>
   )
 }
---
+++ components/ProjectsGrid/ProjectsGrid.tsx
@@ -0,0 +1,8 @@
+export const ProjectsGrid = () => {
+    return (
+        <div>
+            <h2>Projects</h2>
+            <div>Element</div>
+        </div>
+    )
+}
