46cee87c631aa5039287fe380e034015dc4560f1
Timothy Russo <russotimothy@live.it>
2023-08-24 08:55:18 +0200

    feat: style projects cards

--- components/ProjectsGrid/ProjectsGrid.tsx
+++ components/ProjectsGrid/ProjectsGrid.tsx
@@ -5,7 +5,7 @@ export const ProjectsGrid = () => {
   return (
     <div>
       <h2>Projects</h2>
-      <ul>
+      <ul className="grid grid-cols-2 gap-7">
         {projects.map((project) => (
           <ProjectCard
             key={project.id}
