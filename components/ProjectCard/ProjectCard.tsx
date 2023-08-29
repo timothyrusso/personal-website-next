fc16ee4303b9c0546affa5417e01c6acb824e00f
Timothy Russo <russotimothy@live.it>
2023-08-29 18:31:42 +0200

    feat: add hover to the project card

--- components/ProjectCard/ProjectCard.tsx
+++ components/ProjectCard/ProjectCard.tsx
@@ -12,7 +12,7 @@ export const ProjectCard: FC<PrjectCardProps> = ({
   description,
 }) => {
   return (
-    <li className="flex justify-start flex-col text-xl font-medium px-5 rounded-xl min-h-project-card bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500">
+    <li className="flex justify-start flex-col text-xl font-medium px-5 rounded-xl min-h-project-card bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 gradient-background-hover">
       <a href={link} target="_blank">
         <h2 className="mb-3 mt-3 underline">{title}</h2>
         <p className="mb-3 text-base">{description}</p>
