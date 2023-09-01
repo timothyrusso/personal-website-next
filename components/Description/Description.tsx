7ec57c23c4fa96f8c8c18c3df699be0b81c473e3
Timothy Russo <russotimothy@live.it>
2023-09-01 09:23:58 +0200

    fix lint errors

--- components/Description/Description.tsx
+++ components/Description/Description.tsx
@@ -1,17 +1,17 @@
 export const Description = () => {
   return (
     <p className="text-xl font-medium">
-      I’m a{' '}
+      I&apos;m a{' '}
       <span className="bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text text-transparent font-bold gradient-text-hover">
         Frontend Engineer
       </span>{' '}
       with a background in eCommerce, Quality Assurance, Application Delivery,
-      Project Management, and Trade Marketing. Holding a bachelor's degree in
-      Economics and Business Management, I have also completed the Practicum
+      Project Management, and Trade Marketing. Holding a bachelor&apos;s degree
+      in Economics and Business Management, I have also completed the Practicum
       online coding bootcamp for software development. As a curious individual,
-      I'm passionate about coding and enjoy experimenting with new technologies,
-      which has helped me build a strong foundation in problem-solving and Agile
-      methodologies.
+      I&apos;m passionate about coding and enjoy experimenting with new
+      technologies, which has helped me build a strong foundation in
+      problem-solving and Agile methodologies.
     </p>
   );
 };
--- components/Title/Title.tsx
+++ components/Title/Title.tsx
@@ -1,3 +1,3 @@
 export const Title = () => {
-  return <h1 className="text-4xl font-bold">Hi 👋 I'm Timothy Russo</h1>;
+  return <h1 className="text-4xl font-bold">Hi 👋 I&apos;m Timothy Russo</h1>;
 };
