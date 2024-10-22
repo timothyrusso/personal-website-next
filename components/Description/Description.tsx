9ff868aee225c7923a04174f9b89d524932b65ad
Timothy Russo <russotimothy@live.it>
2024-10-22 08:45:03 +0200

    feat: update description

--- components/Description/Description.tsx
+++ components/Description/Description.tsx
@@ -3,11 +3,11 @@ export const Description = () => {
     <p className="text-lg font-medium bg-description p-5 rounded-xl">
       I&apos;m a{' '}
       <span className="bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text text-transparent font-bold gradient-text-hover cursor-progress">
-        Frontend Engineer
+        Frontend and Mobile Engineer
       </span>{' '}
       with a background in eCommerce, Quality Assurance, Application Delivery,
       Project Management, and Trade Marketing. Holding a bachelor&apos;s degree
-      in Economics and Business Management, I have also completed the Practicum
+      in Economics and Business Management, I have also completed the tripleten
       online coding bootcamp for software development. As a curious individual,
       I&apos;m passionate about coding and enjoy experimenting with new
       technologies, which has helped me build a strong foundation in
