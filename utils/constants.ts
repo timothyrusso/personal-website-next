ddc5aacd07d03af606e1e176f7db2c70ce76f2e9
Timothy Russo <russotimothy@live.it>
2025-05-04 15:12:07 +0200

    feat: update projects

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,60 +1,48 @@
 export const sideProjects = [
-  {
-    title: 'flappy-bird',
-    link: 'https://github.com/timothyrusso/FlappyBird',
-    description:
-      'A small game project that replicates Flappy Bird in React Native, using Expo, Reanimated 3, and Skia',
-    id: 1,
-  },
-  {
-    title: 'todo-cli',
-    link: 'https://github.com/timothyrusso/todo-cli',
-    description:
-      'Command line tool made with Node.js where you can add, remove and see some statistics about your colorful tasks',
-    id: 2,
-  },
-  {
-    title: 'crwn-clothing',
-    link: 'https://github.com/timothyrusso/crwn-clothing',
-    description:
-      'Ecommerce web application made with React.js, Typescript, Redux and Firebase',
-    id: 3,
-  },
-  {
-    title: 'news-explorer',
-    link: 'https://github.com/timothyrusso/news-explorer-frontend',
-    description:
-      'Full stack application where users can search news from a public API and save them in their profiles',
-    id: 4,
-  },
-  {
-    title: 'react-around',
-    link: 'https://github.com/timothyrusso/react-around-api-full',
-    description:
-      'Social network full stack application where users can share, add, remove, or like photos',
-    id: 5,
-  },
-  {
-    title: 'learning-how-to-learn',
-    link: 'https://github.com/timothyrusso/web_project_1',
-    description: 'Landing page with some cool css effects',
-    id: 6,
-  },
+	{
+		title: "flappy-bird",
+		link: "https://github.com/timothyrusso/FlappyBird",
+		description:
+			"A small game project that replicates Flappy Bird in React Native, using Expo, Reanimated 3, and Skia",
+		id: 1,
+	},
+
+	{
+		title: "ai-travel-planner",
+		link: "https://github.com/timothyrusso/ai-travel-planner-app",
+		description:
+			"A smart travel planning assistant that leverages Google Gemini AI to create personalized trip itineraries",
+		id: 2,
+	},
+	{
+		title: "Chingu Dashboard",
+		link: "https://github.com/chingu-x/chingu-dashboard",
+		description:
+			"An open source dashboard for Chingu's community to manage their projects and tasks",
+		id: 3,
+	},
+	{
+		title: "todo-cli",
+		link: "https://github.com/timothyrusso/todo-cli",
+		description:
+			"Command line tool made with Node.js where you can add, remove and see some statistics about your colorful tasks",
+		id: 4,
+	},
 ];
 
 export const professionalProjects = [
-  {
-    title: 'ChorusLife App',
-    link: 'https://apps.apple.com/it/app/choruslife/id6443739126',
-    description:
-      'A React Native application designed to manage services and activities for smart districts.',
-    id: 1,
-  },
-  {
-    title: 'Feelspace App',
-    link: 'https://apps.apple.com/it/app/feelspace/id1574724719',
-    description:
-      'A React Native application that helps companies and businesses manage their digital assets.',
-    id: 2,
-  },
+	{
+		title: "ChorusLife App",
+		link: "https://apps.apple.com/it/app/choruslife/id6443739126",
+		description:
+			"A React Native application designed to manage services and activities for smart districts.",
+		id: 1,
+	},
+	{
+		title: "Feelspace App",
+		link: "https://apps.apple.com/it/app/feelspace/id1574724719",
+		description:
+			"A React Native application that helps companies and businesses manage their digital assets.",
+		id: 2,
+	},
 ];
